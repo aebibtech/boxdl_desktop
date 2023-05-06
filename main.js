9c116e07d59be2590035368bb08f5f10ea065f4b
Paul Abib Camano <paul.camano@elinnovtech.com>
2023-05-06 19:02:11 +0800

    Add electron-squirrel-startup

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+if (require('electron-squirrel-startup')) return;
+
 const { app, BrowserWindow, ipcMain, dialog, shell } = require('electron');
 const path = require('path');
 const puppeteer = require('puppeteer');
